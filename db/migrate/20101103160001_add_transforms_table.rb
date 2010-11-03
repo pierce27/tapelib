02f155bd369d5ceec3620e175658d20a85bcd1d1
bonzo27 <aspierce27@gmail.com>
2010-11-03 12:06:23 -0400

    Create migration for adding transform table

---
+++ db/migrate/20101103160001_add_transforms_table.rb
@@ -0,0 +1,18 @@
+class AddTransformsTable < ActiveRecord::Migration
+  def self.up
+    create_table :transforms do |table|
+      table .column :server, 	:string
+      table.column  :server_t, 	:string
+      table.column  :bformat, 	:integer
+      table.column :bformat_t, 	:string
+      table.column :bclient, 	:integer
+      table.column :bclient_t, 	:string
+      table.column :btype,     	:integer
+      table.column :btype_t, 	:string
+      table.column :battr,	:integer
+      table.column :battr_t,	:string		
+  end
+
+  def self.down
+  end
+end
