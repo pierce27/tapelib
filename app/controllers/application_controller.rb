f61e9aa0e25160bbf20ef8eaa7f9b61894b1b424
Andrew Pierce <aspierce27@gmail.com>
2010-11-09 17:09:26 -0500

    Started security measures. Must be logged in to access other pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,15 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  before_filter :authenticate, :only => :index
+#  before_filter :authenticate, :except => :login
   session :on
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   protected
   def authenticate
-        unless session
-                flash[:notice] = "Invalid User/Password"
+        if session[:user_id] == nil
+                flash[:notice] = "YOU MUST BE LOGGED IN TO DO THAT"
                 redirect_to :controller => 'user', :action=> 'login'
         end
    end
