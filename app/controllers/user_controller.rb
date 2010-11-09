fe120f59c643cf97e5ee8935777eea916fd98f94
Andrew Pierce <aspierce27@gmail.com>
2010-11-09 16:30:54 -0500

    Added a way for admin to create delete and edit users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -71,4 +71,31 @@ end
     end
   end
 
+  def users
+    @users = User.all
+
+    respond_to do |format|
+	format.html
+	format.xml {render :xml => @users }
+    end
+  end
+ 
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        flash[:notice] = 'User was successfully updated.'
+        format.html { redirect_to :action => 'users' }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
 end
