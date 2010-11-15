1fe60efc5db68e88a2bce1955627c5d00f3a4074
Andrew Pierce <aspierce27@gmail.com>
2010-11-15 15:58:52 -0500

    Fixed validity, added ultrabac, fixed a link

--- app/models/tape.rb
+++ app/models/tape.rb
@@ -11,7 +11,7 @@ class Tape < ActiveRecord::Base
        self.find(:all, :group => :voltag, :conditions => ["id like ? and  bsid like ? and server like ? and voltag like ? and bformat like ? and btype like ? and bclient like ? and note like ? and testcase like ? ","%#{params[:tape][:id]}%", "%#{params[:tape][:bsid]}%", "%#{params[:tape][:server]}%", "%#{params[:tape][:voltag]}%", "%#{params[:tape][:bformat]}%", "%#{params[:tape][:btype]}%", "%#{params[:tape][:bclient]}%", "%#{params[:tape][:note]}%", "%#{params[:tape][:testcase]}%"])
  end
 
-  BFORMATS = ['COMMVAULT', 'LEGATO', 'TIVOLI', 'ARCSERVE', 'BACKUPEXEC', 'NETBACKUP']
+  BFORMATS = ['COMMVAULT', 'LEGATO', 'TIVOLI', 'ARCSERVE', 'MTF', 'NETBACKUP', 'ULTRABAC']
   
   BTYPES = ['FULL', 'INCREMENTAL', 'DIFFERENTIAL', 'LEVEL 1', 'LEVEL 2', 'LEVEL 3', 'LEVEL 4', 'LEVEL 5', 'LEVEL 6', 'LEVEL 7', 'LEVEL 8', 'LEVEL 9', 'USER', 'UNKNOWN']
 
