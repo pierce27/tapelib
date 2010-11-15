117eb89daa3b76a3095fa32ea608c41cf1a81db5
Andrew Pierce <aspierce27@gmail.com>
2010-11-15 15:59:26 -0500

    Fixed validity, added ultrabac, fixed a link

--- app/models/tape.rb
+++ app/models/tape.rb
@@ -1,7 +1,12 @@
 class Tape < ActiveRecord::Base
   
  validates_numericality_of :testcase, :on => :save
- 
+ validates_presence_of :bsid, :on => :save
+ validates_presence_of :server, :on => :save
+ validates_presence_of :bformat, :on => :save 
+ validates_presence_of :btype, :on => :save
+ validates_presence_of :bclient, :on => :save
+ validates_presence_of :voltag, :on => :save
  
  def self.query(params)
    self.find(:all,   :order => :voltag, :conditions => ["id like ? and  bsid like ? and server like ? and voltag like ? and bformat like ? and btype like ? and bclient like ? and note like ? and testcase like ? ","%#{params[:tape][:id]}%", "%#{params[:tape][:bsid]}%", "%#{params[:tape][:server]}%", "%#{params[:tape][:voltag]}%", "%#{params[:tape][:bformat]}%", "%#{params[:tape][:btype]}%", "%#{params[:tape][:bclient]}%", "%#{params[:tape][:note]}%", "%#{params[:tape][:testcase]}%"])
