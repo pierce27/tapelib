8e910463be696edde28a8881456545e5dd8b6f26
Andrew Pierce <aspierce27@gmail.com>
2011-02-09 12:20:43 -0500

    Fixed controller

--- app/controllers/tapes_controller.rb
+++ app/controllers/tapes_controller.rb
@@ -86,7 +86,7 @@ class TapesController < ApplicationController
 
 
   def updatetape
-    @tapes = Tape.find(:all, :order => :voltag,  :conditions => ["voltag like ?", "%%"])
+    @tapes = Tape.find(:all, :order => :voltag,  :conditions => ["voltag like ?", "%#{params[:tape][:voltag]}%"])
     
    @tapes.each do |tape|  
       tape.update_attributes(params[:tape])
